bb3e27f2727793d97823cd110fe99e8b0f59301f
Amaurys De Los Santos Mendez <131612197+AmaurysM@users.noreply.github.com>
2023-10-09 20:10:50 -0400

    Created a managerView
    Created a basic manager view to be changed, just making it so people can actually work on different parts of the project.

---
+++ src/main/java/com/example/payrollapplication/controllers/managerController.java
@@ -0,0 +1,27 @@
+package com.example.payrollapplication.controllers;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+public class managerController {
+
+    @FXML
+    private MFXButton EmployeesButton;
+
+    @FXML
+    private MFXButton LogoutButton;
+
+    @FXML
+    private MFXButton NotesButton;
+
+    @FXML
+    private MFXButton PayrollButton;
+
+    @FXML
+    private MFXButton SettingsButton;
+
+    @FXML
+    private Circle profilePictureCircle;
+
+}
