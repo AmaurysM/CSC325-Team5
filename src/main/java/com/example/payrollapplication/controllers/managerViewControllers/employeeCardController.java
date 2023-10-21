3687985dc436767ffb8136b3541121737715f576
Amaurys De Los Santos <amaurysdlsm@gmail.com>
2023-10-21 19:54:50 -0400

    Created the employee tab in manager
    This entire page will be changed so I didn't add that much. When you add players to the userbag it just displays it when you start the page.

--- src/main/java/com/example/payrollapplication/controllers/managerViewControllers/employeeCardController.java
+++ src/main/java/com/example/payrollapplication/controllers/managerViewControllers/employeeCardController.java
@@ -1,11 +1,16 @@
 package com.example.payrollapplication.controllers.managerViewControllers;
 
+import com.example.payrollapplication.model.User;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
 
-public class employeeCardController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class employeeCardController implements Initializable {
 
     @FXML
     private Label UserNameLabel;
@@ -16,4 +21,32 @@ public class employeeCardController {
     @FXML
     private Circle profilePictureCircle;
 
+    public Label getUserNameLabel() {
+        return UserNameLabel;
+    }
+
+    public void setUserNameLabel(Label userNameLabel) {
+        UserNameLabel = userNameLabel;
+    }
+
+    public HBox getParentUserCardHBox() {
+        return parentUserCardHBox;
+    }
+
+    public void setParentUserCardHBox(HBox parentUserCardHBox) {
+        this.parentUserCardHBox = parentUserCardHBox;
+    }
+
+    public Circle getProfilePictureCircle() {
+        return profilePictureCircle;
+    }
+
+    public void setProfilePictureCircle(Circle profilePictureCircle) {
+        this.profilePictureCircle = profilePictureCircle;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- src/main/java/com/example/payrollapplication/controllers/managerViewControllers/employeesTabController.java
+++ src/main/java/com/example/payrollapplication/controllers/managerViewControllers/employeesTabController.java
@@ -1,11 +1,20 @@
 package com.example.payrollapplication.controllers.managerViewControllers;
 
+import com.example.payrollapplication.App;
+import com.example.payrollapplication.model.UserBag;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXScrollPane;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.VBox;
 
-public class employeesTabController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class employeesTabController implements Initializable{
 
     @FXML
     private VBox UserCardHolderVBox;
@@ -19,4 +28,63 @@ public class employeesTabController {
     @FXML
     private VBox parentVBox;
 
+
+    @FXML
+    private MFXScrollPane mainmfxScrollPane;
+
+    public VBox getUserCardHolderVBox() {
+        return UserCardHolderVBox;
+    }
+
+    public void setUserCardHolderVBox(VBox userCardHolderVBox) {
+        UserCardHolderVBox = userCardHolderVBox;
+    }
+
+    public MFXButton getMainButton() {
+        return mainButton;
+    }
+
+    public void setMainButton(MFXButton mainButton) {
+        this.mainButton = mainButton;
+    }
+
+    public MFXTextField getMainTextField() {
+        return mainTextField;
+    }
+
+    public void setMainTextField(MFXTextField mainTextField) {
+        this.mainTextField = mainTextField;
+    }
+
+    public VBox getParentVBox() {
+        return parentVBox;
+    }
+
+    public void setParentVBox(VBox parentVBox) {
+        this.parentVBox = parentVBox;
+    }
+
+    public MFXScrollPane getMainmfxScrollPane() {
+        return mainmfxScrollPane;
+    }
+
+    public void setMainmfxScrollPane(MFXScrollPane mainmfxScrollPane) {
+        this.mainmfxScrollPane = mainmfxScrollPane;
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        UserBag.getUserBag().forEach( (e)->{
+
+            try {
+                FXMLLoader employeesCardLoader = new FXMLLoader(App.class.getResource("employeeCard-View.fxml"));
+                employeesCardLoader.load();
+                UserCardHolderVBox.getChildren().add((employeesCardLoader.getRoot()));
+
+                ((employeeCardController)(employeesCardLoader.getController())).getUserNameLabel().setText(e.getName());
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        );
+    }
 }
