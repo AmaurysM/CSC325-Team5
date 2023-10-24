02c0432dd86dbe39a390de79208703c8fff55ac9
Amaurys De Los Santos Mendez <amaurysdlsm@gmail.com>
2023-10-24 14:41:26 -0400

    Added the ability to change between the views
    created a class to manage the main scene. This class is in charge of changing the views.

--- src/main/java/com/example/payrollapplication/App.java
+++ src/main/java/com/example/payrollapplication/App.java
@@ -1,5 +1,6 @@
 package com.example.payrollapplication;
 
+import com.example.payrollapplication.controllers.ScreenController;
 import com.example.payrollapplication.model.UserBag;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,14 +13,17 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("manager-View.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login-View.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 820, 340);
-        stage.setTitle("Pay Roll!");
+        stage.setTitle("PayRoll!!");
 
+        ScreenController screenController = new ScreenController(scene);
+        screenController.addScreen("loginScreen", FXMLLoader.load(getClass().getResource( "login-View.fxml" )));
+        screenController.addScreen("managerView", FXMLLoader.load(getClass().getResource( "manager-View.fxml" )));
+        screenController.addScreen("employeeView", FXMLLoader.load(getClass().getResource( "employee-View.fxml" )));
 
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
